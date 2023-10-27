1a444eb0d33c12bdf0645a5284e147c51fda64b4
MarkSuttirak <116248336+MarkSuttirak@users.noreply.github.com>
2023-10-27 09:00:39 +0000

    Commit Codes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Dashboard from './pages/dashboard'
 import DashboardNew from './pages/dashboardnew'
 import Settings from './pages/settings'
+import { SelectForm } from './pages/test'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path='/old' element={<Dashboard />}/>
         <Route path='/' element={<DashboardNew />}/>
         <Route path="/settings" element={<Settings />}/>
+        <Route path="/test" element={<SelectForm />}/>
       </Routes>
     </BrowserRouter>
   )

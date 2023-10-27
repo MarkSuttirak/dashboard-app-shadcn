1c74f45d1fd3a99f78a0da69c3afdf045ada0b7e
MarkSuttirak <suttirak.ch@zaviago.com>
2023-10-27 16:49:25 +0700

    Add routes file

---
+++ src/routes.jsx
@@ -0,0 +1,56 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+// layouts
+import DashboardLayout from './layouts/DashboardLayout';
+// import LogoOnlyLayout from './layouts/LogoOnlyLayout';
+// pages
+import withAuthenticationRequired from './utils/withAuthenticationRequired';
+import Dashboard from './pages/dashboard';
+import Welcome from './pages/register/welcome';
+import ChangeDomain from './pages/changeDomain';
+import Callback from './callback';
+import Partial, { PhoneVerification } from './pages/register/partial';
+import OtherInfo from './pages/register/otherInfo';
+import InstanceConfig from './pages/register/instanceConfig';
+
+
+// ----------------------------------------------------------------------
+
+export default function Router() {
+    return useRoutes([
+        {
+            path: '/dashboard',
+            element: withAuthenticationRequired(DashboardLayout),
+            children: [
+                { path: 'instance-configuration', element: <InstanceConfig /> },
+                { path: 'app', element: <Dashboard /> },
+                {
+                    path: 'products',
+                    children: [
+                        { path: '/dashboard/products', element: <ChangeDomain /> },
+                        { path: 'add', element: <></> },
+                        { path: 'edit/:id', element: <></> },
+                    ],
+                },
+            ],
+        },
+        {
+            path: '/',
+            // element: <LogoOnlyLayout />,
+            children: [
+                { path: '/', element: <Navigate to="/dashboard/app" /> },
+                { path: 'login', element: <Welcome /> },
+                { path: 'callback', element: <Callback /> },
+                {
+                    path: 'partial/:key',
+                    element: <Partial />,
+                    children: [
+                        { path: 'phone-verification', element: <PhoneVerification /> },
+                        { path: 'other-info', element: <OtherInfo /> },
+                    ]
+                },
+                { path: '*', element: <Navigate to="/404" /> },
+            ],
+        },
+        { path: '*', element: <Navigate to="/404" replace /> },
+    ]);
+}
